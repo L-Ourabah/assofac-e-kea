d5c6a4f9c2826f619ee12d752b2db92c5c8a6438
AaronSaada <127952327+AaronSaada@users.noreply.github.com>
2023-06-05 15:53:36 +0200

    Create script.js
    Page de chargement

---
+++ script.js
@@ -0,0 +1,19 @@
+function demarrage() {
+  let chargement = setTimeout(affichageDeLaPage, 2000);
+}
+
+function affichageDeLaPage() {
+  document.getElementById("loader-fullscreen").style.display = "none";
+  document.getElementById("conteneur-chargement").style.display = "block";
+}
+
+
+const burgerMenu = document.querySelector("#burger-menu");
+const burgerMenuNav = document.querySelector("#burger-menu-nav");
+
+burgerMenu.addEventListener("click", () => {
+  burgerMenuNav.classList.toggle("active");
+  burgerMenu.classList.toggle("active");
+  burgerMenu.style.zIndex = "101";
+  burgerMenu.classList.toggle("burger-menu-fixed");
+})
