7be65073b2bfbe3e81a98239d8a39f4249c9b3f9
Ourabah-Lounnas <134400675+L-Ourabah@users.noreply.github.com>
2023-06-14 10:43:52 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -1,14 +1,15 @@
 //code tabs
+// créer une fonction openMode qui pour parametre modeName
 
 function openMode(ModeName) {
 
   var i;
 
-  var x = document.getElementsByClassName("Mode");
+  var x = document.getElementsByClassName("Mode"); // stock dans la variable x qui l'elemnt qui a pour nom Mode
 
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  for (i = 0; i < x.length; i++) { // donne 0 a i ; tant que i est strictement inferieur au nombre d'element contenu dans x j'incremente i
+    x[i].style.display = "none"; // applique a l'element x qui a pour index i 
   }
-  document.getElementById(ModeName).style.display = "block";
+  document.getElementById(ModeName).style.display = "block"; // qui pou id l'element en parametre
 
 }
