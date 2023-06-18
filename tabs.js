ece9ab7312d028d7c17204ca3bfdf23dba97f70a
ourabah <ourabah.lou@hotmail.com>
2023-06-18 16:51:33 +0200

    Update tabs.js

--- tabs.js
+++ tabs.js
@@ -10,6 +10,6 @@ function openMode(ModeName) {
   for (i = 0; i < x.length; i++) { // donne 0 a i ; tant que i est strictement inferieur au nombre d'element contenu dans x j'incremente i
     x[i].style.display = "none"; // applique a l'element x qui a pour index i 
   }
-  document.getElementById(ModeName).style.display = "block"; // qui pou id l'element en parametre
+  document.getElementById(ModeName).style.display = "block"; // applique un display block pour id l'element en parametre
 
-}
+}r
